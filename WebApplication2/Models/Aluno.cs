b457c92a520e7f39f33c2373998266a8af240d2c
NathanG16 <nathangomescarneiro95@gmail.com>
2025-09-24 21:58:11 -0300

    atualização do Aluno

---
+++ WebApplication2/Models/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace WebApplication2.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Matricula {  get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Data_De_Nascimento {  get; set; }
+        public string Cep { get; set; }
+        public string Endereco {  get; set; }
+        public string Bairro {  get; set; }
+        public string Cidade { get; set; }
+        public string Numero { get; set; }
+    }
+}
